6b9635b25bb7fc36176c3d46fa558231f765e84d
errload <errload@mail.ru>
2022-08-22 15:49:23 +0300

    hystory start

--- install/script.js
+++ install/script.js
@@ -27,15 +27,32 @@ define(['jquery', 'underscore', 'twigjs', 'lib/components/base/modal'], function
                         $modal_body
                             .trigger('modal:loaded')
                             .html(`
-                            <div class="modal__main-block" style="width: 100%; min-height: 0px;">
-                                <h2 class="modal-body__caption head_2">Запуск таймера</h2>
-                            </div>
-                        `)
+                                <div class="modal__main-block" style="width: 100%; min-height: 0px;">
+                                    <h2 class="modal-body__caption head_2">Запуск таймера</h2>
+                                </div>
+                            `)
                             .trigger('modal:centrify')
                             .append('');
                     },
                     destroy: function () {}
                 });
+
+                /* ############################################################### */
+
+                // история
+                $('.modal__main-block').css('position', 'relative');
+                hystoryLink = `
+                    <a href="#" class="hystory__link" style="
+                        text-decoration: none;
+                        color: #1375ab;
+                        top: 0;
+                        right: 0;
+                        position: absolute;
+                    ">История</a>
+                `;
+                $('.modal__main-block').append(hystoryLink);
+
+                /* ############################################################### */
 
                 // ссылка на проект
                 var linkProject = `<a href="#" class="modal__link-project" style="
@@ -69,6 +86,7 @@ define(['jquery', 'underscore', 'twigjs', 'lib/components/base/modal'], function
                 // добавляем ссылку проекта и ссылку (изменить)
                 $('.modal__main-block').append(linkProjectWrapper);
                 $('.modal__link-project__wrapper').append(linkProject);
+                $('.modal__link-project').attr('href', $('.modal__link-project').text());
                 $('.modal__link-project').after(changeLinkProject);
 
                 // удаляем ссылку и вставляем поле ввода ссылки
@@ -90,6 +108,7 @@ define(['jquery', 'underscore', 'twigjs', 'lib/components/base/modal'], function
 
                         $('.modal__link-project__wrapper').append(linkProject);
                         $('.modal__link-project').text($('.modal__input__link-project').val().trim() + ' ');
+                        $('.modal__link-project').attr('href', $('.modal__link-project').text());
                         $('.modal__link-project').after(changeLinkProject);
                         $('.modal__input__link-project').remove();
                         $('.change__link-project').bind('click', changeLink);
@@ -97,6 +116,46 @@ define(['jquery', 'underscore', 'twigjs', 'lib/components/base/modal'], function
                 }
                 $('.change__link-project').bind('click', changeLink);
 
+                /* ############################################################### */
+
+                // ссылка на задачу
+                var inputLinkTask = Twig({ ref: '/tmpl/controls/input.twig' }).render({
+                    name: 'modal-input-link-task',
+                    class_name: 'modal__input__link-task',
+                    value: '',
+                    placeholder: 'вставьте ссылку на задачу'
+                });
+
+                var linkTaskWrapper = `<div class="modal__link-task__wrapper" style="width: 100%; margin-top: 10px;">
+                    <span style="width: 100%;">Ссылка на задачу:</span><br/>
+                </div>`;
+
+                // добавляем ссылку задачу
+                $('.modal__main-block').append(linkTaskWrapper);
+                $('.modal__link-task__wrapper').append(inputLinkTask);
+                $('.modal__input__link-task').css({
+                    'width': '100%',
+                    'margin-top': '3px'
+                });
+
+                /* ############################################################### */
+
+                // таймер
+                var timerBtn = Twig({ ref: '/tmpl/controls/button.twig' }).render({
+                    class_name: 'timer__btn',
+                    text: 'Старт',
+                });
+
+                var timerWrapper = `<div class="modal__timer__wrapper" style="width: 100%; margin-top: 20px;">
+                    <span style="font-size: 24px;">19:08:23</span>
+                </div>`;
+
+                $('.modal__main-block').append(timerWrapper);
+                $('.modal__timer__wrapper').append(timerBtn);
+                $('.timer__btn').css({
+                    'margin-left': '20px',
+                    'margin-top': '-8px'
+                });
 
 
 
@@ -104,30 +163,100 @@ define(['jquery', 'underscore', 'twigjs', 'lib/components/base/modal'], function
 
 
 
-                // кнопки Сохранить и Отменить
-                var startBtn = Twig({ ref: '/tmpl/controls/button.twig' }).render({
-                        class_name: 'modal__startBtn-timer',
-                        text: 'Запустить'
-                    }),
-                    closeBtn = Twig({ ref: '/tmpl/controls/cancel_button.twig' }).render({
-                        class_name: 'modal__closeBtn-timer',
+
+
+                /* ############################################################### */
+
+                // запуск истории
+                $('.hystory__link').unbind('click');
+                $('.hystory__link').bind('click', function (e) {
+                    e.preventDefault();
+
+                    new Modal({
+                        class_name: 'hystory__timer',
+                        init: function ($modal_body) {
+                            var $this = $(this);
+                            $modal_body
+                                .trigger('modal:loaded')
+                                .html(`
+                                    <div class="modal__hystory-block" style="width: 100%; min-height: 550px;">
+                                        <h2 class="modal-body__caption head_2">История</h2>
+                                    </div>
+                                `)
+                                .trigger('modal:centrify')
+                                .append('');
+                        },
+                        destroy: function () {}
+                    });
+
+                    // депозит
+                    var inputHystoryDeposit = Twig({ ref: '/tmpl/controls/input.twig' }).render({
+                        name: 'modal-input-hystory-deposit',
+                        class_name: 'modal__input__hystory-deposit',
+                        value: '20000',
+                        placeholder: 'укажите депозит'
+                    });
+
+                    var HystoryDepositWrapper = `<div class="modal__hystory-deposit__wrapper" style="width: 100%; margin-top: 20px;">
+                        <span >Депозит:</span>
+                    </div>`;
+
+                    $('.modal__hystory-block').append(HystoryDepositWrapper);
+                    $('.modal__hystory-deposit__wrapper').append(inputHystoryDeposit);
+
+
+
+
+
+
+
+                    $('.modal__hystory-block').append('<div class="modal__bottom" style="position: absolute; height: 70px; width: 100%"></div>');
+
+                    // кнопка Закрыть
+                    var cancelBtn = Twig({ ref: '/tmpl/controls/cancel_button.twig' }).render({
+                            class_name: 'modal__cancelBtn-hystory',
+                            text: 'Закрыть'
+                        }),
+                        btnWrapper = '<div class="modal-body__actions" style="width: 100%; text-align: right;"></div>';
+
+                    $('.modal__hystory-block').append(btnWrapper);
+                    $('.modal__hystory-block .modal-body__actions').append(cancelBtn);
+                    $('.modal__hystory-block .modal-body__actions').css('margin-top', '20px');
+                    $('.modal__hystory-block').css('position', 'relative');
+                    $('.modal__hystory-block .modal-body__actions').css({
+                        'left': '0px',
+                        'bottom': '0px',
+                        'position': 'absolute'
+                    });
+                });
+
+
+
+
+
+
+
+
+                // кнопка Закрыть
+                var cancelBtn = Twig({ ref: '/tmpl/controls/cancel_button.twig' }).render({
+                        class_name: 'modal__cancelBtn-timer',
                         text: 'Закрыть'
                     }),
                     btnWrapper = '<div class="modal-body__actions" style="width: 100%; text-align: right;"></div>';
 
                 $('.modal__main-block').append(btnWrapper);
-                $('.modal-body__actions').append(startBtn);
-                $('.modal-body__actions').append(closeBtn);
+                $('.modal-body__actions').append(cancelBtn);
                 $('.modal-body__actions').css('margin-top', '20px');
 
                 // чтобы модальное окно не прижималось книзу (не смог исправить стандартный вариант)
                 // $('.modal__main-block').append('<div class="modal__bottom" style="position: absolute; height: 70px; width: 100%"></div>');
 
-                // нажатие на кнопку Запустить
-                $('.modal__startBtn-timer').unbind('click');
-                $('.modal__startBtn-timer').bind('click', function () {
-                    console.log('click');
-                });
+                // нажатие на кнопку Сохранить
+                // $('.modal__saveBtn-timer').unbind('click');
+                // $('.modal__saveBtn-timer').bind('click', function () {
+                //     console.log('save');
+                //     $('.start__timer').remove();
+                // });
             });
         }
 
